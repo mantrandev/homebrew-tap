2a991eaa049acd7078caac00353ddf6e9d8139da
maybe <120156244+mantrandev@users.noreply.github.com>
2026-08-12 11:00:08 +0700

    feat: mkey 0.0.5, arm64-only

--- Casks/mkey.rb
+++ Casks/mkey.rb
@@ -1,11 +1,14 @@
 cask "mkey" do
-  version "0.0.3"
-  sha256 "9672eecfa0692c1d85548364342a51ca13bf003068f3be4b42fdd070251c590c"
+  version "0.0.5"
+  sha256 "6abec73f7acb3df2b93b487309ca0b47dfe01acad8fde8d1930a9f71c859eb7d"
 
   url "https://github.com/mantrandev/Mkey/releases/download/v#{version}/Mkey-v#{version}.dmg"
   name "Mkey"
   desc "Minimal Vietnamese input method for macOS"
   homepage "https://github.com/mantrandev/Mkey"
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :ventura"
 
   app "Mkey.app"
 
