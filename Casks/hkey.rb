754470d7d2541a648aa276c04b48eebacf8eac92
maybe <120156244+mantrandev@users.noreply.github.com>
2026-08-12 10:28:23 +0700

    feat: add hkey cask

---
+++ Casks/hkey.rb
@@ -0,0 +1,20 @@
+cask "hkey" do
+  version "0.0.4"
+  sha256 "fe31311175995eebb03d76e621556c84123558cae7929124d207a78045c0603b"
+
+  url "https://github.com/mantrandev/HKey/releases/download/v#{version}/HKey-v#{version}.dmg"
+  name "HKey"
+  desc "Minimal Vietnamese input method for macOS"
+  homepage "https://github.com/mantrandev/HKey"
+
+  conflicts_with cask: [
+    "mantrandev/tap/hkey-vni",
+    "mantrandev/tap/hkey-telex",
+  ]
+
+  app "HKey.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.mantrandev.hkey.plist",
+  ]
+end
